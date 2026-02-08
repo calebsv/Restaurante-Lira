23daf795d4231203d0b0a583c506cecb293d68c5
Caleb Lira <contatocaleb2010@gmail.com>
2026-02-08 14:36:46 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('active');
+    }
+  });
+});
+
+reveals.forEach(reveal => {
+  observer.observe(reveal);
+});
